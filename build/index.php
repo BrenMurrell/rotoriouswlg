297f8b9c3507b44540b4fc5efb356856bf660167
BrenMurrell <bren.murrell@nvinteractive.com>
2017-05-18 11:44:04 +1200

    initial commit, holding page

---
+++ build/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Black+Ops+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+    <link rel="stylesheet" href="/wp-content/themes/rwlg/css/main.css">
+  </head>
+  <body>
+    <div class="homepage">
+      <div class="glass"></div>
+      <div class="hp__title">
+        <h1>Rotorious WLG</h1>
+        <h2>Guerilla FPV</h2>
+    </div>
+    <h3 class="comingsoon">Coming soon</h3>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/wp-content/themes/rwlg/js/main.js"></script>
+  </body>
+</html>
---
+++ build/js/main.js
@@ -0,0 +1,5 @@
+$(window).on("load",function() {
+  var doBam = setTimeout(function() {
+    $('.homepage').addClass('bam');
+  },800)
+});
